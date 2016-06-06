e5be873bb1b28250c8092750daa4b92bfce354bc
odakayaka <ayaka.odaka@gmail.com>
2016-06-07 01:14:18 +0900

    Initial Commit

---
+++ GachaTech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GachaTech
+//
+//  Created by 尾高文香 on 2016/06/07.
+//  Copyright © 2016年 com.odakaayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
